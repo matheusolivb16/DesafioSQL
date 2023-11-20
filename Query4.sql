c412525bf1e21b240106cdda645ffec819cc83bb
matheusolivb16 <108027634+matheusolivb16@users.noreply.github.com>
2023-11-20 14:47:15 -0300

    Update Query4.sql

--- Query4.sql
+++ Query4.sql
@@ -1,5 +1,5 @@
-SELECT MONTH(cab.Data)
+SELECT MONTH(cab.Data) as mes
        SUM(det.ValorLiquido),
 FROM FatoDetalhes_DadosModelagem det
 JOIN FatoCabecalho_DadosModelagem cab on  cab.CupomID = det.CupomID
-GROUP BY cab.Data
+GROUP BY mes
